5ae89a2549fa8f82882aa60d8c56b57eada9c992
风痕 <fenghen.ld@dtyunxi.com>
2020-08-31 09:14:33 +0800

    31

---
+++ dw-logger/src/test/java/com/duoduo/dwlogger/DwLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoduo.dwlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
